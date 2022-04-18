070ae9728b75ee0b4923f4faa091b685953e61b3
kurpoon <103907244+kurpoon@users.noreply.github.com>
2022-04-18 10:53:23 +1000

    Update student.py

--- student.py
+++ student.py
@@ -237,7 +237,7 @@ lossFunc = loss()
 # ################# The following determines training options ###################
 # ###############################################################################
 
-trainValSplit = 0.8
+trainValSplit = 0.99 #0.8
 batchSize = 32
 epochs = 10
 optimiser = toptim.SGD(net.parameters(), lr=0.01)
